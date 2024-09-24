abd3c69ab296053dd6ed189912e216cbe87473cb
tranlynhathao <haotran04022005@gmail.com>
2024-09-24 12:33:13 +0700

    pages: add 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "../styles/scss/404.module.scss";
+const Custom404 = () => {
+    return (
+        <div style={{ textAlign: 'center', paddingTop: '100px' }}> {}
+            <h1>404 - Page Not Found</h1>
+            <img src="/img/button_h.png" alt="Logo" style={{ margin: '0 auto' }} />
+        </div>
+    );
+}
+
+export default Custom404;
