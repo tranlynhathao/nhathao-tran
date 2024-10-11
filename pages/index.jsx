a6750ddd59222fb53464a5e4ab858ad03ced38c1
tranlynhathao <haotran04022005@gmail.com>
2024-10-11 15:48:28 +0700

    adjust index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import Hero from '../components/sections/index/hero';
 import Looking from '../components/sections/index/looking';
 import About from '../components/sections/index/about';
 // import Technical from '../components/sections/index/technical';
-import Career from '../components/sections/index/career';
+// import Career from '../components/sections/index/career';
 // import FeaturedProjects from '../components/sections/projects/featured';
 import Color from '../components/utils/page.colors.util';
 import colors from '../content/index/_colors.json';
